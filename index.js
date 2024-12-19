f1548535a1ddbfb95c91aab04078dacee9a0563d
FujiwaraChoki <sami.hindi@renuo.ch>
2024-12-19 17:24:06 +0100

    chore: Update dependencies & set URL to monthly calendar view

--- index.js
+++ index.js
@@ -1,171 +1,179 @@
-// include the Node.js 'path' module at the top of your file
-const path = require('path')
-const { app, BrowserWindow, Menu, nativeImage, session, Tray } = require('electron')
-const { windowStateKeeper } = require("./stateKeeper")
+const path = require("path");
+const {
+  app,
+  BrowserWindow,
+  Menu,
+  nativeImage,
+  session,
+  Tray,
+} = require("electron");
+const { windowStateKeeper } = require("./stateKeeper");
 // const isDevelopment = process.env.NODE_ENV !== "production";
 const isDevelopment = require("electron-is-dev");
-const { readFileSync } = require('fs');
+const { readFileSync } = require("fs");
 
-// const iconPath = isDevelopment ? path.join('assets', 'icon.png') : path.resolve(app.getAppPath(), 'assets', 'icon.png');
 const iconPath = path.join(
-    isDevelopment ? process.cwd() + "/resources" : process.resourcesPath,
-    "icon.ico"
-)
-console.log(`iconPath: ${iconPath}`)
+  isDevelopment ? process.cwd() + "/resources" : process.resourcesPath,
+  "icon.ico"
+);
+console.log(`iconPath: ${iconPath}`);
 
-const CALENDER_HOME = `https://calendar.google.com/calendar/u/0/r/agenda`
+const CALENDER_HOME = "https://calendar.google.com/calendar/u/0/r/week";
 
 const setHomeCss = (mainWindow) => {
-    const stylesPath = path.join(
-        isDevelopment ? process.cwd() + "/resources" : process.resourcesPath,
-        "styles.css"
-    )
-    const styles = readFileSync(stylesPath).toString()
-    mainWindow.webContents.insertCSS(styles)
-    console.log(`CSS Set`)
-}
+  const stylesPath = path.join(
+    isDevelopment ? process.cwd() + "/resources" : process.resourcesPath,
+    "styles.css"
+  );
+  const styles = readFileSync(stylesPath).toString();
+  mainWindow.webContents.insertCSS(styles);
+  console.log(`CSS Set`);
+};
 
 // modify your existing createWindow() function
 const createWindow = () => {
-    const mainWindow = new BrowserWindow({
-        height: 480,
-        width: 320,
+  const mainWindow = new BrowserWindow({
+    height: 480,
+    width: 320,
 
-        maximizable: false,
-        minimizable: false,
-        icon: iconPath,
+    maximizable: false,
+    minimizable: false,
+    icon: iconPath,
 
-        skipTaskbar: !isDevelopment,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
+    skipTaskbar: !isDevelopment,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  mainWindow.loadURL(CALENDER_HOME);
+  mainWindow.webContents.setWindowOpenHandler(({ url }) => {
+    mainWindow.loadURL(url);
+    return { action: "deny" };
+  });
+
+  mainWindow.webContents.on("will-navigate", (e, url) => {
+    console.log(`will-navigate`, url);
+    // if (url.includes(`https://accounts.google.com/CheckCookie`)) {
+    //     setTimeout(() => {
+    //         mainWindow.loadURL(CALENDER_HOME)
+    //     }, 1500)
+    // }
+    if (url === CALENDER_HOME || url.includes(CALENDER_HOME)) {
+      setHomeCss(mainWindow);
+    }
+  });
+
+  setHomeCss(mainWindow);
+  windowStateKeeper("main")
+    .then((mwk) => {
+      if (mwk) {
+        const { x, y, width, height } = mwk;
+        if (x !== undefined && y !== undefined && width && height) {
+          mainWindow.setBounds({ x, y, width, height });
         }
+        mwk.track(mainWindow);
+      }
     })
-
-    mainWindow.loadURL(CALENDER_HOME);
-    mainWindow.webContents.setWindowOpenHandler(({ url }) => {
-        mainWindow.loadURL(url)
-        return { action: 'deny' };
+    .catch((e) => {
+      console.log(`Error in windowStateKeeper:`, e);
     });
 
-    mainWindow.webContents.on("will-navigate", (e, url) => {
-        console.log(`will-navigate`, url)
-        // if (url.includes(`https://accounts.google.com/CheckCookie`)) { 
-        //     setTimeout(() => {
-        //         mainWindow.loadURL(CALENDER_HOME)
-        //     }, 1500)
-        // } 
-        if (url === CALENDER_HOME || url.includes(CALENDER_HOME)) {
-            setHomeCss(mainWindow) 
-        }
-    })
+  if (isDevelopment) {
+    mainWindow.webContents.openDevTools({ mode: "undocked" });
+  } else {
+    mainWindow.setMenu(null);
+    app.setLoginItemSettings({
+      openAtLogin: true,
+    });
+  }
 
-    setHomeCss(mainWindow)
-    windowStateKeeper('main')
-        .then((mwk) => {
-            if (mwk) {
-                const { x, y, width, height } = mwk;
-                if (x !== undefined && y !== undefined && width && height) {
-                    mainWindow.setBounds({ x, y, width, height });
-                }
-                mwk.track(mainWindow);
-            }
-        })
-        .catch((e) => {
-            console.log(`Error in windowStateKeeper:`, e);
-        });
+  const isSingleInstance = app.requestSingleInstanceLock();
 
-    if (isDevelopment) {
-        mainWindow.webContents.openDevTools({ mode: "undocked" });
-    }
-    else {
-        mainWindow.setMenu(null)
-        app.setLoginItemSettings({
-            openAtLogin: true,
-        });
-    }
+  if (!isSingleInstance) {
+    app.quit();
+    mainWindow.focus();
+  } else {
+    app.on("second-instance", (event, commandLine, workingDirectory) => {
+      // Someone tried to run a second instance, we should focus our window.
+      if (mainWindow) {
+        if (mainWindow.isMinimized()) mainWindow.restore();
+        mainWindow.focus();
+      }
+    });
+  }
 
-    const isSingleInstance = app.requestSingleInstanceLock()
-
-    if (!isSingleInstance) {
-        app.quit()
-        mainWindow.focus()
-    } else {
-        app.on('second-instance', (event, commandLine, workingDirectory) => {
-            // Someone tried to run a second instance, we should focus our window.
-            if (mainWindow) {
-                if (mainWindow.isMinimized()) mainWindow.restore()
-                mainWindow.focus()
-            }
-        })
-    }
-
-    createTray(mainWindow)
-}
+  createTray(mainWindow);
+};
 
 const createTray = (mainWindow) => {
-    // Load your tray icon image
-    const trayIcon = nativeImage.createFromPath(iconPath);
+  // Load your tray icon image
+  const trayIcon = nativeImage.createFromPath(iconPath);
 
-    // Create the tray
-    const tray = new Tray(trayIcon);
+  // Create the tray
+  const tray = new Tray(trayIcon);
 
-    // Create a context menu
-    const contextMenu = Menu.buildFromTemplate([
-        {
-            label: 'Reload', click: () => {
-                // mainWindow.reload()
-                // mainWindow.loadURL(CALENDER_HOME);
-                // setHomeCss(mainWindow)
+  // Create a context menu
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Reload",
+      click: () => {
+        // mainWindow.reload()
+        // mainWindow.loadURL(CALENDER_HOME);
+        // setHomeCss(mainWindow)
 
-                app.relaunch()
-                app.exit()
-            }
-        },
-        {
-            label: 'Logout', click: () => {
-                session.defaultSession.clearStorageData()
-                    .then(() => {
-                        mainWindow.loadURL(CALENDER_HOME)
-                    })
-                    .catch((e) => {
-                        console.log(`clearStorageData`, e)
-                    })
-            }
-        },
-        {
-            label: 'Exit', click: () => {
-                app.quit()
-            }
-        }
-    ]);
+        app.relaunch();
+        app.exit();
+      },
+    },
+    {
+      label: "Logout",
+      click: () => {
+        session.defaultSession
+          .clearStorageData()
+          .then(() => {
+            mainWindow.loadURL(CALENDER_HOME);
+          })
+          .catch((e) => {
+            console.log(`clearStorageData`, e);
+          });
+      },
+    },
+    {
+      label: "Exit",
+      click: () => {
+        app.quit();
+      },
+    },
+  ]);
 
-    // Set the context menu for the tray
-    tray.setContextMenu(contextMenu);
+  // Set the context menu for the tray
+  tray.setContextMenu(contextMenu);
 
-    // Add a tooltip (optional)
-    tray.setToolTip('GCW');
-    tray.setTitle("GCW")
+  // Add a tooltip (optional)
+  tray.setToolTip("GCW");
+  tray.setTitle("GCW");
 
-    // Show the tray icon
-    tray.on('click', () => {
-        mainWindow.show();
-    });
+  // Show the tray icon
+  tray.on("click", () => {
+    mainWindow.show();
+  });
 
-    console.log(`Tray icon added`)
+  console.log(`Tray icon added`);
 };
 
 app.whenReady().then(() => {
-    createWindow()
-})
+  createWindow();
+});
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
 
-app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+app.on("activate", () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
